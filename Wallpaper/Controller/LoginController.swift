b5139ce728f254a820b502875a2e708f9748ce93
CoderWeiLee <coderweilee@gmail.com>
2021-01-27 17:25:15 +0800

    壁纸登录页面重写

--- Wallpaper/Controller/LoginController.swift
+++ Wallpaper/Controller/LoginController.swift
@@ -13,7 +13,7 @@ class LoginController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //0.设置导航栏和文字标题
-        title = "登录"
+        title = ""
         tabBarItem.title = ""
         navigationController?.isNavigationBarHidden = false
         view.backgroundColor = .white
@@ -21,68 +21,61 @@ class LoginController: UIViewController {
         //隐藏分割线
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
+        let bgImageV = UIImageView()
+        view.addSubview(bgImageV)
+        bgImageV.snp.makeConstraints { (make) in
+            make.edges.equalTo(view)
+        }
         
-        //添加返回按钮
-        let backBtn = UIButton(type: .custom)
-        backBtn.adjustsImageWhenHighlighted = false
-        backBtn.setImage(UIImage(named: "browser-back-black"), for: .normal)
-        backBtn.addTarget(self, action: #selector(backAction), for: .touchUpInside)
-        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: backBtn)
-        
-        
-        //添加注册按钮
-        let registerBtn = UIButton(type: .custom)
-        registerBtn.addTarget(self, action: #selector(registerAction), for: .touchUpInside)
-        registerBtn.adjustsImageWhenHighlighted = false
-        registerBtn.setTitle("注册", for: .normal)
-        registerBtn.setTitleColor(UIColor(named: "indicatorColor"), for: .normal)
-        registerBtn.titleLabel?.font = UIFont.systemFont(ofSize: 16)
-        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: registerBtn)
-        
-        //添加头像
-        let imgV = UIImageView()
-        imgV.image = UIImage(named: "AppIcon")
-        view.addSubview(imgV)
-        imgV.snp.makeConstraints { (make) in
-            make.width.height.equalTo(80)
-            make.top.equalTo(view).offset(30)
+        //欢迎登陆
+        let welcomeLabel = UILabel()
+        welcomeLabel.text = "欢迎登陆"
+        welcomeLabel.font = UIFont.systemFont(ofSize: 30)
+        view.addSubview(welcomeLabel)
+        welcomeLabel.snp.makeConstraints { (make) in
             make.centerX.equalTo(view)
+            make.top.equalTo(view).offset(kScreenH * 0.3)
         }
         
-        //用户名标签
-        let nameLabel = UILabel()
-        nameLabel.text = "用户名:"
-        view.addSubview(nameLabel)
-        nameLabel.snp.makeConstraints { (make) in
-            make.left.equalTo(view).offset(50)
-            make.top.equalTo(imgV.snp.bottom).offset(45)
+        //手机号码文本输入框
+        let phoneText = UITextField()
+        phoneText.placeholder = "请输入手机号码"
+        view.addSubview(phoneText)
+        phoneText.snp.makeConstraints { (make) in
+            make.left.equalTo(view).offset(20)
+            make.top.equalTo(view).offset(kScreenH * 0.5)
+            make.right.equalTo(view).offset(-20)
         }
         
-        //用户名文本输入框
-        let nameText = UITextField()
-        nameText.placeholder = "请输入用户名"
-        view.addSubview(nameText)
-        nameText.snp.makeConstraints { (make) in
-            make.left.equalTo(nameLabel.snp.right)
-            make.centerY.equalTo(nameLabel)
+        let phoneLine = UIView()
+        phoneLine.backgroundColor = UIColor(white: 0.3, alpha: 0.3)
+        view.addSubview(phoneLine)
+        phoneLine.snp.makeConstraints { (make) in
+            make.left.equalTo(view).offset(10)
+            make.top.equalTo(phoneText.snp.bottom).offset(5)
+            make.height.equalTo(2)
+            make.right.equalTo(view).offset(-10)
         }
         
-        //密码标签
-        let pwdLabel = UILabel()
-        pwdLabel.text = "密  码:"
-        view.addSubview(pwdLabel)
-        pwdLabel.snp.makeConstraints { (make) in
-            make.left.equalTo(nameLabel)
-            make.top.equalTo(nameLabel.snp.bottom).offset(45)
-        }
         
         //用户名文本输入框
-        let pwdText = UITextField()
-        pwdText.placeholder = "请输入密码"
-        view.addSubview(pwdText)
-        pwdText.snp.makeConstraints { (make) in
-            make.left.equalTo(pwdLabel.snp.right)
-            make.centerY.equalTo(pwdLabel)
+        let codeText = UITextField()
+        codeText.placeholder = "请输入验证码"
+        view.addSubview(codeText)
+        codeText.snp.makeConstraints { (make) in
+            make.left.equalTo(phoneText)
+            make.top.equalTo(phoneLine.snp.bottom).offset(30)
+            make.right.equalTo(phoneText).offset(-80)
+        }
+        
+        let codeLine = UIView()
+        codeLine.backgroundColor = UIColor(white: 0.3, alpha: 0.3)
+        view.addSubview(codeLine)
+        codeLine.snp.makeConstraints { (make) in
+            make.left.equalTo(view).offset(10)
+            make.top.equalTo(codeText.snp.bottom).offset(5)
+            make.height.equalTo(2)
+            make.right.equalTo(view).offset(-10)
         }
         
         //登录按钮
@@ -96,24 +89,15 @@ class LoginController: UIViewController {
         loginBtn.layer.masksToBounds = true
         view.addSubview(loginBtn)
         loginBtn.snp.makeConstraints { (make) in
-            make.left.equalTo(nameLabel)
-            make.top.equalTo(pwdLabel.snp.bottom).offset(80)
-            make.right.equalTo(view).offset(-50)
+            make.left.equalTo(view).offset(30)
+            make.right.equalTo(view).offset(-30)
             make.height.equalTo(44)
+            make.top.equalTo(codeLine.snp.bottom).offset(20)
         }
         
     }
     
-    //返回
-    @objc func backAction() {
-        navigationController?.popViewController(animated: true)
-    }
-    
-    //注册
-    @objc func registerAction() {
-        let registerVc = RegisterController()
-        navigationController?.pushViewController(registerVc, animated: true)
-    }
+  
     
     //登录
     @objc func loginAction() {
