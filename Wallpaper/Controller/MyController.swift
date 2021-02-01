a2bcdfe7651d1f70379569675683da1645a6476c
CoderWeiLee <coderweilee@gmail.com>
2021-02-01 10:15:51 +0800

    个人中心重写

--- Wallpaper/Controller/MyController.swift
+++ Wallpaper/Controller/MyController.swift
@@ -10,137 +10,113 @@ import SnapKit
 class MyController: UIViewController {
     override func viewDidLoad() {
         var bezierPath: UIBezierPath!
+        var ovalImageView: UIImageView!
+        var loginContainerView: UIView!
         var shapeLayer: CAShapeLayer!
+        var gradientLayer: CAGradientLayer!
+        var loginLabel: UILabel!
+        var loginTipsLabel: UILabel!
+        var loginBtn: UIButton!
+        var sepLine: UIView!
         super.viewDidLoad()
         //0.设置导航栏和文字标题
         title = "个人中心"
         tabBarItem.title = ""
         navigationController?.isNavigationBarHidden = false
-        view.backgroundColor = .white
+        view.backgroundColor = UIColor(hexString: "#F7F7F7")
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(named: "indicatorColor") ?? .green, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16)]
         //隐藏分割线
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
-        bezierPath = UIBezierPath()
-        bezierPath.move(to: CGPoint(x: 0, y: 0))
-        bezierPath.addLine(to: CGPoint(x: kScreenW, y: 0))
-        bezierPath.addLine(to: CGPoint(x: kScreenW, y: 300))
-        bezierPath.addQuadCurve(to: CGPoint(x: 0, y: 300), controlPoint: CGPoint(x: kScreenW * 0.5, y: 350))
-        bezierPath.close()
-//        bezierPath.stroke()
         
-//        let topView = UIView(frame: CGRect(x: 0, y: 0, width: kScreenW, height: 400))
-//        topView.addGradientColor(colors: [UIColor(hexString: "#FF62A5"), UIColor(hexString: "#FF632E")], locations: [0,1], direction: .Horizontal, targetView: topView)
-//        view.addSubview(topView)
-        shapeLayer = CAShapeLayer()
+        //0.添加顶部的渐变色图层
+        bezierPath = UIBezierPath.init()
+        bezierPath.move(to: CGPoint.init(x: 0, y: 0))
+        bezierPath.addLine(to: CGPoint.init(x: kScreenW, y: 0))
+        bezierPath.addLine(to: CGPoint(x: kScreenW, y: 180))
+        bezierPath.addQuadCurve(to: CGPoint(x: 0, y: 180), controlPoint: CGPoint(x: kScreenW * 0.5, y: 230))
+        shapeLayer = CAShapeLayer.init()
         shapeLayer.path = bezierPath.cgPath
-        shapeLayer.fillColor = UIColor.red.cgColor
-        shapeLayer.strokeColor = UIColor.yellow.cgColor
-        shapeLayer.lineWidth = 1
-        shapeLayer.masksToBounds = true
-        view.layer.addSublayer(shapeLayer)
+        gradientLayer = CAGradientLayer()
+        gradientLayer.frame = CGRect(x: 0, y: 0, width: kScreenW, height: 230)
+        gradientLayer.colors = [UIColor(hexString: "#FF62A5").cgColor, UIColor(hexString: "#FF632E").cgColor]
+        gradientLayer.locations = [0,1]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 1)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 0)
+        gradientLayer.mask = shapeLayer
+        self.view.layer.addSublayer(gradientLayer)
         
-//        //1.添加子控件
-//        let imgV = UIImageView()
-//        imgV.image = UIImage(named: "menu_default_avatar")
-//        imgV.layer.cornerRadius = 48
-//        imgV.layer.masksToBounds = true
-//        view.addSubview(imgV)
-//        imgV.snp.makeConstraints { (make) in
-//            make.centerX.equalTo(view)
-//            make.top.equalTo(view).offset(30)
-//            make.width.equalTo(96)
-//            make.height.equalTo(96)
-//        }
-//
-//        let loginLabel = UILabel()
-//        loginLabel.text = "点击登录"
-//        loginLabel.textColor = UIColor(named: "menu_default_avatar")
-//        loginLabel.font = UIFont.systemFont(ofSize: 16)
-//        view.addSubview(loginLabel)
-//        loginLabel.snp.makeConstraints { (make) in
-//            make.centerX.equalTo(view)
-//            make.top.equalTo(imgV.snp.bottom).offset(15)
-//        }
-//
-//        //签到领福利
-//        let signInTitle = UILabel()
-//        signInTitle.text = "签到领福利"
-//        signInTitle.font = UIFont.systemFont(ofSize: 14)
-//        view.addSubview(signInTitle)
-//        signInTitle.snp.makeConstraints { (make) in
-//            make.left.equalTo(imgV.snp.right).offset(20)
-//            make.top.equalTo(imgV).offset(20)
-//        }
-//
-//        //签到红包的小图标
-//        let signInImg = UIImageView()
-//        signInImg.image = UIImage(named: "hgg_redbag")
-//        view.addSubview(signInImg)
-//        signInImg.snp.makeConstraints { (make) in
-//            make.left.equalTo(signInTitle.snp.right).offset(10)
-//            make.top.equalTo(signInTitle)
-//        }
-//
-//        //提示label 今天还没签到哦/明日记得来签到哦
-//        let signInTips = UILabel()
-//        signInTips.text = "今天还没签到哦 >"
-//        signInTips.font = UIFont.systemFont(ofSize: 14)
-//        signInTips.textColor = UIColor.orange
-//        view.addSubview(signInTips)
-//        signInTips.snp.makeConstraints { (make) in
-//            make.left.equalTo(signInTitle)
-//            make.top.equalTo(signInTitle.snp.bottom).offset(10)
-//        }
-//
-//        //签到按钮
-//        let signInBtn = UIButton(type: .custom)
-//        signInBtn.addTarget(self, action: #selector(signInAction), for: .touchUpInside)
-//        view.addSubview(signInBtn)
-//        signInBtn.snp.makeConstraints { (make) in
-//            make.left.top.equalTo(signInTitle)
-//            make.right.bottom.equalTo(signInTips)
-//        }
-//
-//        let loginBtn = UIButton(type: .custom)
-//        loginBtn.addTarget(self, action: #selector(loginAction), for: .touchUpInside)
-//        view.addSubview(loginBtn)
-//        loginBtn.snp.makeConstraints { (make) in
-//            make.top.equalTo(view).offset(30)
-//            make.left.equalTo(imgV).offset(-10)
-//            make.right.equalTo(imgV).offset(10)
-//            make.bottom.equalTo(loginLabel).offset(10)
-//        }
-//
-//        let table = UITableView()
-//        table.isScrollEnabled = false
-//        table.separatorStyle = .none
-//        table.delegate = self
-//        table.tableFooterView = UIView()
-//        table.showsHorizontalScrollIndicator = false
-//        table.showsVerticalScrollIndicator = false
-//        table.dataSource = self
-//        table.register(MyCell.self, forCellReuseIdentifier: "cell")
-//        view.addSubview(table)
-//        table.snp.makeConstraints { (make) in
-//            make.top.equalTo(loginBtn.snp.bottom)
-//            make.left.right.bottom.equalTo(view)
-//        }
-//
-//        //读取缓存中的记录
-//        if let signDates = UserDefaults.standard.object(forKey: "signDate") as? [Date] {
-//           if (signDates.filter {
-//                Calendar.current.isDate($0, inSameDayAs: Date())
-//           }.count > 0) {
-//            signInTips.text = "今天已签到>"
-//           }
-//        }
+        //1.添加顶部的登录容器
+        loginContainerView = UIView()
+        loginContainerView.layer.cornerRadius = 4
+        loginContainerView.backgroundColor = .white
+        view.addSubview(loginContainerView)
+        loginContainerView.snp.makeConstraints { (make) in
+            make.left.equalTo(view).offset(18)
+            make.right.equalTo(view).offset(-16)
+            make.top.equalTo(view).offset(86)
+            make.height.equalTo(180)
+        }
+        
+        //1.1添加用户头像
+        ovalImageView = UIImageView()
+        ovalImageView.image = UIImage(named: "Oval")
+        loginContainerView.addSubview(ovalImageView)
+        ovalImageView.snp.makeConstraints { (make) in
+            make.width.height.equalTo(94)
+            make.left.equalTo(loginContainerView).offset(19)
+            make.top.equalTo(loginContainerView).offset(20)
+        }
+        
+        //1.2添加点击登录
+        loginLabel = UILabel()
+        loginLabel.text = "点击登录"
+        loginLabel.textColor = UIColor(hexString: "#4A4A4A")
+        loginLabel.font = UIFont.systemFont(ofSize: 24)
+        loginContainerView.addSubview(loginLabel)
+        loginLabel.snp.makeConstraints { (make) in
+            make.left.equalTo(ovalImageView.snp.right).offset(12)
+            make.top.equalTo(loginContainerView).offset(29)
+        }
+        
+        //1.3添加登录提示文字 登录后可以收藏美图哦
+        loginTipsLabel = UILabel()
+        loginTipsLabel.text = "登录后可以收藏美图哦"
+        loginTipsLabel.textColor = UIColor(hexString: "#9B9B9B")
+        loginTipsLabel.font = UIFont.systemFont(ofSize: 16)
+        loginContainerView.addSubview(loginTipsLabel)
+        loginTipsLabel.snp.makeConstraints { (make) in
+            make.left.equalTo(loginLabel)
+            make.top.equalTo(loginLabel.snp.bottom).offset(2)
+        }
+        
+        //1.4添加登录按钮
+        loginBtn = UIButton(type: .custom)
+        loginBtn.addTarget(self, action: #selector(loginAction), for: .touchUpInside)
+        loginContainerView.addSubview(loginBtn)
+        loginBtn.snp.makeConstraints { (make) in
+            make.left.top.right.equalTo(loginContainerView)
+            make.height.equalTo(100)
+        }
+        
+        //1.5添加分割线
+        sepLine = UIView()
+        sepLine.backgroundColor = UIColor(hexString: "#F5F5F5")
+        loginContainerView.addSubview(sepLine)
+        sepLine.snp.makeConstraints { (make) in
+            make.left.right.equalTo(loginContainerView)
+            make.height.equalTo(1)
+            make.top.equalTo(ovalImageView.snp.bottom).offset(9)
+        }
+       
+             
+ 
     }
     
     @objc func loginAction() {
         let loginVc = LoginController()
         loginVc.hidesBottomBarWhenPushed = true
-        navigationController?.pushViewController(loginVc, animated: true)
+        present(loginVc, animated: true, completion: nil)
     }
     
     //签到
@@ -148,33 +124,5 @@ class MyController: UIViewController {
         let signinVc = SigninController()
         signinVc.hidesBottomBarWhenPushed = true
         navigationController?.pushViewController(signinVc, animated: true)
-    }
-}
-
-extension MyController: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MyCell
-        cell.selectionStyle = .none
-        cell.textLabel?.font = UIFont.systemFont(ofSize: 13)
-        switch indexPath.row {
-        case 0:
-            cell.imgV.image = UIImage(named: "item-btn-like-on")
-            cell.titleLabel.text = "我的喜欢"
-        case 1:
-            cell.imgV.image = UIImage(named: "item-btn-like-on")
-            cell.titleLabel.text = "清除缓存"
-            cell.detailLabel.text = "9.7M"
-        case 2:
-            cell.imgV.image = UIImage(named: "item-btn-like-on")
-            cell.titleLabel.text = "设置壁纸"
-        default:
-            cell.imgV.image = UIImage(named: "item-btn-like-on")
-            cell.titleLabel.text = "退出"
-        }
-        return cell
     }
 }
